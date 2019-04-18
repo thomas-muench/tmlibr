9d19f89edb6cbdbad9e51ad255629293d0dfda6a
Thomas Münch <tmuench@awi.de>
2019-04-18 10:12:43 +0200

    Added 'pdf2png' function.

---
+++ R/pdf2png.R
@@ -0,0 +1,18 @@
+##' pdf to png conversion
+##'
+##' Convert an existing pdf file to a png file using the Mac OS X command line
+##' tool \code{sips}.
+##' @param file the root file name of the pdf file to convert, i.e. the filename
+##' without the \code{.pfd} extension.
+##' @return the default output from \code{\link{system}}.
+##' @author Thomas Münch
+##' @source https://robservatory.com/use-sips-to-quickly-easily-and-freely-convert-image-files/
+pdf2png <- function(file) {
+
+    cmd <- sprintf("sips -s format png %s --out %s",
+                   paste0(file, ".pdf"), paste0(file, ".png"))
+    
+    system(cmd)
+
+}
+
